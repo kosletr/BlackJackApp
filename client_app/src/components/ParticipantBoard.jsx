022512f2a80f1da2b5a8abd5b560b5535168154f
Kostas Letros <kosletr@gmail.com>
2022-09-02 11:47:48 +0300

    Add colors to win-defeat-tie messages.

--- client_app/src/components/ParticipantBoard.jsx
+++ client_app/src/components/ParticipantBoard.jsx
@@ -4,19 +4,25 @@ import Card from './Card';
 export default function ParticipantBoard({ playerId, participant, turn, showFlippedCard = false }) {
     if (!participant || Object.keys(participant).length === 0) return;
 
+    const outcomeStyle = "outcome"
+        + (participant.outcome === "WIN" ? " outcome--win" : "")
+        + (participant.outcome === "DEFEAT" ? " outcome--defeat" : "")
+        + (participant.outcome === "TIE" ? " outcome--tie" : "");
+
     return (
         <div className={"participant" + (participant.id === turn ? " participant__info--active" : "")}>
             <div className={"participant__info"}>
                 {participant.name || "Dealer"}
-                <span style={{ color: "red" }}>{participant.clientId === playerId ? " (You)" : ""}</span>
+                <span style={{ color: "greenyellow" }}>{participant.clientId === playerId ? " (You)" : ""}</span>
             </div>
             <div className='cards' >
                 {participant.cards.map((c, idx) => <Card key={idx} rank={c.rank} suit={c.suit} />)}
                 {showFlippedCard && <Card />}
             </div>
-            {participant.outcome && <div className="outcome">
-                {participant.outcome}
-            </div>
+            {participant.outcome &&
+                <div className={outcomeStyle}>
+                    {participant.outcome}
+                </div>
             }
             {(participant.totalAmount || participant.totalAmount === 0) &&
                 <div>
